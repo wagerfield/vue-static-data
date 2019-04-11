c1a320994fe373ef339dd11559f398876218de82
Bakhirev <Aleksey.Bakhirev@billing.ru>
2019-04-11 14:50:19 +0300

    Add component context to static data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function install(Vue) {
   Vue.mixin({
     beforeCreate: function() {
       var staticData = this.$options.staticData
-      if (typeof staticData === "function") staticData = staticData()
+      if (typeof staticData === "function") staticData = staticData.apply(this)
       if (isObject(staticData)) Object.assign(this, staticData)
     }
   })
